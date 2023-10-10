b5aa10fd500c8d535e011750c85bea0d3600843a
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2023-10-10 13:00:37 +0530

    Update SettingsPrefsFragment.java

--- FinalFiles/SettingsPrefsFragment.java
+++ FinalFiles/SettingsPrefsFragment.java
@@ -1,3 +1,5 @@
+SearchHistoryAdapter searchHistoryAdapter = new SearchHistoryAdapter();
+ViewHolde res;
 private void initSuperPrivacyPrefsCallbacks(){
      final Preference pref = getPreference(getString(R.string.pref_super_privacy));
 
@@ -6,13 +8,28 @@ private void initSuperPrivacyPrefsCallbacks(){
      pref.setOnPreferenceChangeListener(
         (preference,newValue) ->
         {
-            boolean newVal = (Boolean) newVal;
+            boolean newVal = (Boolean) newValue;
             if(isSuperPrivacyEnabled != newVal){
                  SearchRecents.clear();
-                 getAdapter().notifyDataSetChanged();
+                 searchHistoryAdapter.notifyDataSetChanged();
 /*                  Config.setSuperPrivacyEnabled(newVal);
                  Utils.(newVal,requireActivity()); */
-            }
+            }else{
+                 res = new ViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_search_recent, viewGroup, false));
+                 res.mText.setOnClickListener(new View.OnClickListener()
+                 {
+                    @Override
+                    public void onClick(View v)
+                    {
+                      mSearchToolbarController.setQuery(res.mText.getText());
+                    }
+                  });  
+
+                 or 
+
+                  searchHistoryAdapter.getIsEnabled(Config.isSuperPrivacyEnabled);
+                 
+            }     
             return true;
         });
 }
